451c9a98c5d933ff754ec3ac6a42118048d7d2ba
bhaskarkr <officialbhaskarkumar@gmail.com>
2022-03-17 09:50:51 +0530

    Update score-of-parentheses.cpp

--- score-of-parentheses.cpp
+++ score-of-parentheses.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    
+    //     int scoreOfParentheses(string s) {
+    //     stack<int> stk;
+    //     int curr = 0;
+    //     for(char c : s) {
+    //         if(c == '(') {
+    //             stk.push(curr);
+    //             curr = 0;
+    //         } else {
+    //             curr += stk.top() + max(1, curr);
+    //             stk.pop();
+    //         }
+    //     }
+    //     return curr;
+    // }
+    
     int scoreOfParentheses(string s) {
         int openBck = 0;
         int ans = 0;
