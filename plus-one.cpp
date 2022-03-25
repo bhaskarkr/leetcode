6ab13fef44d72f083a27c4078b9c03281dc7a2ab
bhaskarkr <officialbhaskarkumar@gmail.com>
2022-03-25 15:41:16 +0530

    Create plus-one.cpp

---
+++ plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int carry = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            carry += digits[i];
+            digits[i] = carry % 10;
+            carry/=10;
+        }
+        if(carry)
+            digits.insert(digits.begin(), carry);
+        return digits;
+    }
+};
