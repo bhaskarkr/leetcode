d5bf90426c6724861e11cb9076a4dc336c99d01f
bhaskarkr <officialbhaskarkumar@gmail.com>
2022-03-28 21:35:24 +0530

    Update number-of-good-ways-to-split-a-string.cpp

--- number-of-good-ways-to-split-a-string.cpp
+++ number-of-good-ways-to-split-a-string.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     int numSplits(string s) {
+        vector<int> preCount(s.length());
         unordered_map<int, int> uniqueChar;
+        int i = 0;
         for(char c : s) {
             uniqueChar[c]++;
+            preCount[i++] = uniqueChar.size();
         }
         int ans = 0;
         unordered_map<int, int> tempChar;
-        for(char c : s) {
+        for(i = s.length() - 1; i > 0; i--) {
+            char c = s[i];
             tempChar[c]++;
-            uniqueChar[c]--;
-            if(uniqueChar[c] == 0)
-                uniqueChar.erase(c);
-            if(tempChar.size() == uniqueChar.size())
+            if(tempChar.size() == preCount[i-1])
                 ans++;
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int numSplits(string s) {
+//         unordered_map<int, int> uniqueChar;
+//         for(char c : s) {
+//             uniqueChar[c]++;
+//         }
+//         int ans = 0;
+//         unordered_map<int, int> tempChar;
+//         for(char c : s) {
+//             tempChar[c]++;
+//             uniqueChar[c]--;
+//             if(uniqueChar[c] == 0)
+//                 uniqueChar.erase(c);
+//             if(tempChar.size() == uniqueChar.size())
+//                 ans++;
+//         }
+//         return ans;
+//     }
+// };
+
+
