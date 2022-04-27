cab4f88aeaafaf4145a7c8bfe53f575b2152d66d
bhaskarkr <officialbhaskarkumar@gmail.com>
2022-04-27 13:24:07 +0530

    Create smallest-string-with-swaps.cpp

---
+++ smallest-string-with-swaps.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> parent;
+    // iterative one
+    // int findParent(int a) {
+    //     while(parent[a] != a) {
+    //         parent[a] = parent[parent[a]];
+    //         a = parent[a];
+    //     }
+    //     return a;
+    // }
+    // recursive one    
+    int findParent(int a) {
+        return a == parent[a] ? a : parent[a] = findParent(parent[a]);
+    }
+    
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        int l = s.length();
+        parent.assign(l, 0);
+        for(int i = 0; i < l; i++)
+            parent[i] = i;
+        for(auto p : pairs) {
+            int i = findParent(p[0]);
+            int j = findParent(p[1]);
+            if(i != j) {
+                if(i > j)
+                    swap(i, j);
+                parent[j] = i;
+            }
+        }
+        // have 2 option here 
+        // 1. make vector of chars and sort in each group and populate accordingly
+        // 2. create a priority queue and pop the min one and keep assign it to positions
+        unordered_map<int, priority_queue<char, vector<char>, greater<char>>> umap;
+        for(int i = 0; i < l; i++) {
+            umap[findParent(i)].push(s[i]);
+        }
+        // not calling again findParent because it was already set while populating map
+        for(int i = 0; i < l; i++) {
+            s[i] = umap[parent[i]].top();
+            umap[parent[i]].pop();
+        }
+        return s;
+    }
+};
