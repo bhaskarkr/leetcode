1e460c73f7a566298ffa42b19ff4f981669c6039
bhaskarkr <officialbhaskarkumar@gmail.com>
2022-06-29 20:08:37 +0530

    queue-reconstruction-by-height

---
+++ queue-reconstruction-by-height.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b) {
+        if(a[0] == b[0])
+            return a[1] < b[1];
+        return a[0] > b[0];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        vector<vector<int>> ans;
+        sort(people.begin(), people.end(), comp);
+        for(auto height : people) {
+            int index = height[1];
+            ans.insert(ans.begin() + index, height);
+        }
+        return ans;
+    }
+};
