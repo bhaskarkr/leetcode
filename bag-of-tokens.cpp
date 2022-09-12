5e3a2f524229bef8014c742330abf36d6e5afa57
bhaskarkr <officialbhaskarkumar@gmail.com>
2022-09-11 23:14:43 -0700

    Create bag-of-tokens.cpp

---
+++ bag-of-tokens.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
+        int i = 0, j = tokens.size() - 1;
+        int curr = 0;
+        int ans = 0;
+        while(i <= j) {
+            if(tokens[i] <= power) {
+                curr++;
+                power -= tokens[i++];
+            } else if(ans > 0) {
+                curr--;
+                power += tokens[j--];
+            } else {
+                break;
+            }
+            ans = max(ans, curr);
+        }
+        
+        return ans;
+    }
+};
