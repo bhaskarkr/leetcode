bf6d814334238a951e2b3a2075bc74e9c6460b29
bhaskarkr <officialbhaskarkumar@gmail.com>
2022-11-24 22:13:57 -0700

    Create number-complement.cpp

---
+++ number-complement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int i = 0;
+        while(i < num) {
+            i = (i << 1) | 1;
+        }
+        return i - num;
+    }
+};
