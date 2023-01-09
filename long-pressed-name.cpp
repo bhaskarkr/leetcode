e789a981693627004f23922eedf0b9282602b71c
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-01-09 15:22:44 -0700

    Create long-pressed-name.cpp

---
+++ long-pressed-name.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int i = 0, j = 0, n = name.length(), m = typed.length();
+        while(j < m) {
+            if(i < n && name[i] == typed[j]) {
+                i++;
+                j++;
+            }
+            else if(j > 0 && typed[j] == typed[j-1])
+                j++;
+            else
+                return false;
+        }
+        return i == n;
+    }
+};
