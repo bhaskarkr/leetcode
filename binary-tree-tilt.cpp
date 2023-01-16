dd740b88689ed9ba5e9ff688b93114607d0d9757
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-01-16 00:18:40 -0700

    Create binary-tree-tilt.cpp

---
+++ binary-tree-tilt.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        int ans = 0;
+        helper(ans, root);
+        return ans;
+    }
+
+    int helper(int &ans, TreeNode* root) {
+        if(!root)
+            return 0;
+        int left = helper(ans, root->left);
+        int right = helper(ans, root->right);
+        ans += abs(right - left);
+        return root->val + left + right;
+    }
+};
