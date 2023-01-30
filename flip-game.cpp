8d921fe31bb3d81f9aea1f1239b0f65ece29b7c2
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-01-29 18:54:04 -0700

    Create flip-game.cpp

---
+++ flip-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string currentState) {
+        vector<string> ans;
+        for(int i = 1; i < currentState.length(); i++) {
+            if(currentState[i] == '+' && currentState[i-1] == '+') {
+                currentState[i] = '-';
+                currentState[i-1] = '-';
+                ans.push_back(currentState);
+                currentState[i] = '+';
+                currentState[i-1] = '+';
+            }
+        }
+        return ans;
+    }
+};
