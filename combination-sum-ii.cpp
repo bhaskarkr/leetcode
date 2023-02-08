b23401c71830153dcef5077ef38b4fa2cf6d3c4e
Bhaskar Kumar <20877284+bhaskarkr@users.noreply.github.com>
2023-02-08 09:17:08 -0700

    Create combination-sum-ii.cpp

---
+++ combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(candidates.begin(), candidates.end());
+        helper(0, candidates, target, 0, temp, ans);
+        return ans;
+    }
+
+    void helper(int index, vector<int> &candidates, int &target, int currSum, vector<int> &currEle, vector<vector<int>> &ans) {
+        if(currSum == target) {
+            ans.push_back(currEle);
+            return;
+        }
+        if(currSum > target || index >= candidates.size())
+            return;
+        for(int i = index; i < candidates.size(); i++) {
+            if(i > index && candidates[i] == candidates[i-1])
+                continue;
+            currEle.push_back(candidates[i]);
+            helper(i + 1, candidates, target, currSum + currEle.back(), currEle, ans);
+            currEle.pop_back();
+        }
+    }
+
+};
