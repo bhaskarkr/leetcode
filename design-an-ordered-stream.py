2d885f37d3fd9f7be5e7ca2572def98d12d4d3a3
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-02-12 21:59:29 -0700

    Create design-an-ordered-stream.py

---
+++ design-an-ordered-stream.py
@@ -0,0 +1,16 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.count = 1
+        self.data = [-1]*1002
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.data[idKey] = value
+        while self.data[self.count] != -1:
+            self.count += 1
+        return self.data[idKey: self.count]
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
