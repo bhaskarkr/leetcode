330672f1c567745ef73d491a00dae392abd08556
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-02-15 17:31:00 -0700

    Create maximum-depth-of-binary-tree.py

---
+++ maximum-depth-of-binary-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    ans = 0
+    def helper(self, root : Optional[TreeNode], depth : int):
+        if not root:
+            self.ans = max(self.ans, depth)
+            return
+        self.helper(root.left, depth + 1)
+        self.helper(root.right, depth + 1)
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        self.helper(root, 0)
+        return self.ans
