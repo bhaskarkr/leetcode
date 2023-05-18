9e0e473cdf199bf9c7a255952a36f631958aa999
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-05-17 20:18:53 -0700

    Create minimum-number-of-vertices-to-reach-all-nodes.cpp

---
+++ minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        unordered_map<int, int> in;
+        vector<int> ans;
+        for(auto edge : edges) {
+            in[edge[1]]++;
+        }
+        for(int i = 0; i < n; i++)
+            if(in[i] == 0)
+                ans.push_back(i);
+        return ans;
+    }
+};
