ef08c3fb277acce717729b4e1e8ff5d839d852c2
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-08-16 15:34:46 -0700

    Update step-by-step-directions-from-a-binary-tree-node-to-another.cpp

--- step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -53,3 +53,53 @@ public:
 
     }
 };
+
+
+
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+*     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+*     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+* };
+*/
+class Solution {
+public:
+string start;
+string dest;
+string path;
+string getDirections(TreeNode* root, int startValue, int destValue) {
+    helper(root, startValue, destValue);
+    int i = 0;
+    while(start[i] == dest[i])
+        i++;
+    dest = dest.substr(i);
+    return string(start.length() - i, 'U') + dest;
+}
+
+void helper(TreeNode* root, int startValue, int destValue) {
+    if(!root || (!start.empty() && !dest.empty())) {
+        return;
+    }
+    if(root->val == startValue) {
+        start = path;
+    }
+    if(root->val == destValue) {
+        dest = path;
+    }
+    if(root->left) {
+        path.push_back('L');
+        helper(root->left, startValue, destValue);
+        path.pop_back();
+    }
+    if(root->right) {
+        path.push_back('R');
+        helper(root->right, startValue, destValue);
+        path.pop_back();
+    }
+}
+};
