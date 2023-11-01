417bbb3f5052aeca8b8afaa79d8b8a846f4e204a
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-10-31 17:30:49 -0700

    Update find-mode-in-binary-search-tree.cpp

--- find-mode-in-binary-search-tree.cpp
+++ find-mode-in-binary-search-tree.cpp
@@ -33,3 +33,41 @@ public:
         return temp;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        unordered_map<int , int> umap;
+        int maxC = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            umap[curr->val]++;
+            maxC = max(maxC, umap[curr->val]);
+            if(curr->left)
+                q.push(curr->left);
+            if(curr->right)
+                q.push(curr->right);
+        }
+        vector<int> temp;
+        for(auto [k, v] : umap) {
+            if(v == maxC)
+                temp.push_back(k);
+        }
+        return temp;
+    }
+};
