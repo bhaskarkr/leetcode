96af9d393c4b1dac27fc17662a07584fef978bc7
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-11-02 08:18:15 -0700

    Update odd-even-jump.cpp

--- odd-even-jump.cpp
+++ odd-even-jump.cpp
@@ -21,3 +21,26 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int oddEvenJumps(vector<int>& arr) {
+        int n = arr.size(), ans = 1;
+        map<int, int> mns;
+        unordered_map<int, pair <bool, bool> > oddEven;
+        mns[arr.back()] = n-1;
+        oddEven[n-1] = {1, 1};
+        for(int i = n - 2; i >= 0; i--) {
+            auto greaterThanEqual = mns.lower_bound(arr[i]);
+            auto smallerThanEqual = mns.upper_bound(arr[i]);
+            if(smallerThanEqual != mns.begin())
+                oddEven[i].second = oddEven[prev(smallerThanEqual)->second].first;
+            if(greaterThanEqual != mns.end())
+                oddEven[i].first = oddEven[greaterThanEqual->second].second;
+            if(oddEven[i].first)
+                ans++;
+            mns[arr[i]] = i;
+        }
+        return ans;
+    }
+};
