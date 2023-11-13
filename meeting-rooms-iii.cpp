6d627a6dc4486e92812ff9521f9c6adef760349d
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-11-13 12:52:53 -0700

    Update meeting-rooms-iii.cpp

--- meeting-rooms-iii.cpp
+++ meeting-rooms-iii.cpp
@@ -37,3 +37,66 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        priority_queue<pair<long, int>, vector<pair<long, int>>, greater<pair<long, int>>> pq;
+        sort(meetings.begin(), meetings.end());
+        for(int i = 0; i < n; i++)
+            pq.push({meetings[0][0], i});
+        int ans = 0, maxi = 0;
+        vector<int> count(n);
+        for(int i = 0; i < meetings.size(); i++) {
+            while(pq.top().first < meetings[i][0]) {
+                int ind = pq.top().second; 
+                pq.pop();
+                pq.push({meetings[i][0], ind});
+            }
+            auto curr = pq.top();
+            pq.pop();
+            curr.first += meetings[i][1] - meetings[i][0];
+            count[curr.second]++;
+            pq.push(curr);
+        }
+        for(int i = 0 ; i < n; i++) {
+            if(maxi < count[i]) {
+                maxi = count[i];
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int mostBooked(int n, vector<vector<int>>& meetings) {
+//         priority_queue<pair<long long, int>> pq;
+//         sort(meetings.begin(), meetings.end());
+//         for(int i = 0; i < n; i++)
+//             pq.push({0, -i});
+//         int ans = 0, maxi = 0;
+//         vector<int> count(n);
+//         for(int i = 0; i < meetings.size(); i++) {
+//             while(-pq.top().first < meetings[i][0]) {
+//                 int ind = pq.top().second; 
+//                 pq.pop();
+//                 pq.push({-meetings[i][0], ind});
+//             }
+//             auto curr = pq.top();
+//             pq.pop();
+//             curr.first -= meetings[i][1] - meetings[i][0];
+//             count[-curr.second]++;
+//             pq.push(curr);
+//         }
+//         for(int i = 0 ; i < n; i++) {
+//             if(maxi < count[i]) {
+//                 maxi = count[i];
+//                 ans = i;
+//             }
+//         }
+//         return ans;
+//     }
+// };
