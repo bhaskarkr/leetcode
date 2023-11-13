5f6e847579efbdcd79a312f1c8c783638699707f
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-11-13 10:37:11 -0700

    Create detonate-the-maximum-bombs.cpp

---
+++ detonate-the-maximum-bombs.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long getDist(vector<int> &a, vector<int> &b) {
+        return (a[0] - b[0]*1L)*(a[0] - b[0]) + (a[1] - b[1]*1L) * (a[1] - b[1]);
+    }
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        vector<vector<int>> distance(bombs.size());
+        int ans = 0;
+        for(int i = 0; i < bombs.size(); i++) 
+            for(int j = i + 1; j < bombs.size(); j++) {
+                long dist = getDist(bombs[i], bombs[j]);
+                if(dist <= bombs[i][2]*(bombs[i][2]*1L))
+                    distance[i].push_back(j);
+                if(dist <= bombs[j][2]*(bombs[j][2]*1L))
+                    distance[j].push_back(i);
+            }
+        
+        queue<int> q;
+        for(int i = 0; i < bombs.size(); i++) {
+            int temp = 0;
+            vector<bool> visited(bombs.size());
+            q.push(i);
+            visited[i] = true;
+            while(!q.empty()) {
+                int ci = q.front();
+                temp++;
+                q.pop();
+                for(int j : distance[ci]) {
+                    if(!visited[j]) {
+                        q.push(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+            ans = max(ans, temp);
+        } 
+        return ans;
+    }
+};
