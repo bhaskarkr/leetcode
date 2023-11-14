00f7f92eae2fe6f17c78dfe5dfcd9a7c29e4d077
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-11-13 19:31:20 -0700

    Update unique-length-3-palindromic-subsequences.cpp

--- unique-length-3-palindromic-subsequences.cpp
+++ unique-length-3-palindromic-subsequences.cpp
@@ -14,3 +14,27 @@ public:
         return count;
     }
 };
+
+
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        unordered_map<char, vector<int>> ans;
+        unordered_map<char, int> temp;
+        int count = 0;
+        for(int i = 0; i < s.length() ; i++) {
+            if(ans[s[i]].size() != 0) {
+                int cc = 0, k = ans[s[i]][0];
+                for(char c = 'a'; c <= 'z'; c++) {
+                    if(upper_bound(ans[c].begin(), ans[c].end(), k) != ans[c].end())
+                        cc++;
+                }
+                temp[s[i]] = cc;
+            }
+            ans[s[i]].push_back(i);
+        }
+        for(auto [c, k] : temp)
+            count += k;
+        return count;
+    }
+};
