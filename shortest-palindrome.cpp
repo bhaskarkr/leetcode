74f71c85d7f1bbf15c7b29bc1a07fdeeea18d733
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-11-22 05:10:35 +0530

    Create shortest-palindrome.cpp

---
+++ shortest-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+    string shortestPalindrome(string ss) {
+        string rr = ss;
+        reverse(rr.begin(), rr.end());
+        string_view s(ss);
+        string_view r(rr);
+        for(int i = 0; i < s.length(); i++) {
+            if(r.substr(i) == s.substr(0, s.length() - i))
+                return rr.substr(0, i) + ss;
+        }
+        return ss;
+    }
+};
