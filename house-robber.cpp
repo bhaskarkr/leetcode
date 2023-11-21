2f1f19c3c3b67f73a6beaaa54b93817966c22b01
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-11-22 00:25:22 +0530

    Create house-robber.cpp

---
+++ house-robber.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i++)
+            nums[i] = max(nums[i] + (i > 1 ? nums[i-2] : 0 ), nums[i-1]);
+        return nums.back();
+    }
+};
