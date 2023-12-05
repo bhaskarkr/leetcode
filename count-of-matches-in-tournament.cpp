6cbb543e1850f2ef3cec385458a150d9d03bc829
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-12-04 17:23:32 -0700

    Create count-of-matches-in-tournament.cpp

---
+++ count-of-matches-in-tournament.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if(n <= 1)
+            return 0;
+        return n % 2 
+                ? (n - 1) / 2 + numberOfMatches((n - 1) / 2 + 1) 
+                : n / 2 + numberOfMatches(n / 2);
+    }
+};
