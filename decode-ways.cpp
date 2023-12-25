a7badf6bf387cb7a0683e13bb2b9694fc3635d5f
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-12-24 19:01:55 -0700

    Update decode-ways.cpp

--- decode-ways.cpp
+++ decode-ways.cpp
@@ -17,3 +17,47 @@ public:
         return DP[n-1];
     }
 };
+
+class Solution {
+public:
+    unordered_map<int, int> dp;
+    int helper(int index, string &s) {
+        if(s.length() > index && s[index] == '0')
+            return 0;
+        if(s.length() - 1 <= index)
+            return 1;
+        int curr = 0, num = 0;
+        if(dp.find(index) != dp.end())
+            return dp[index];
+        curr += helper(index + 1, s);
+        if(index + 1 < s.length() && (s[index] == '1' || (s[index] == '2' && s[index + 1] < '7')))
+            curr += helper(index + 2, s);
+        return dp[index] = curr;
+    }
+    int numDecodings(string s) {
+        return helper(0, s);
+    }
+};
+// class Solution {
+// public:
+//     unordered_map<int, int> dp;
+//     int helper(int index, string &s) {
+//         if(s.length() > index && s[index] == '0')
+//             return 0;
+//         if(s.length() - 1 <= index)
+//             return 1;
+//         int curr = 0, num = 0, end = index + 2;
+//         if(dp.find(index) != dp.end())
+//             return dp[index];
+//         for(;index < end; index++) {
+//             num = num * 10 + (s[index] - '0');
+//             if(num > 26 || num < 1)
+//                 break;
+//             curr += helper(index + 1, s);
+//         }
+//         return dp[index] = curr;
+//     }
+//     int numDecodings(string s) {
+//         return helper(0, s);
+//     }
+// };
