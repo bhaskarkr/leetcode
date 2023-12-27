8157b62ab05a7a42720dfb611378c44ebceb8173
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2023-12-27 11:14:23 -0700

    Update minimum-time-to-make-rope-colorful.cpp

--- minimum-time-to-make-rope-colorful.cpp
+++ minimum-time-to-make-rope-colorful.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
+        int ans = 0, n = neededTime.size(), minCost = 0, sum = 0;
+        for(int i = 0; i < n;) {
+            minCost = neededTime[i];
+            i++;
+            while(i < n && colors[i] == colors[i-1]) {
+                if(neededTime[i] < minCost)
+                    ans += neededTime[i];
+                else {
+                    ans += minCost;
+                    minCost = neededTime[i];
+                }
+                i++;
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
         int n = neededTime.size();
         int ans = 0;
         for(int i = 0; i < n;) {
