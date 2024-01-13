db0b7f07ca29cb774df6c666daf3830454207f89
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-01-12 21:13:14 -0700

    Update frog-jump.cpp

--- frog-jump.cpp
+++ frog-jump.cpp
@@ -24,3 +24,28 @@ public:
         return helper(stones, 1, 1) == 1;
     }
 };
+
+
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+        if(stones[1] > 1)
+            return false;
+        int n = stones.size();
+        vector<vector<int>> DP(n, vector<int>(n + 1));
+        DP[0][1] = 1;
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                int diff = stones[i] - stones[j];
+                if(diff < 0 || diff > n || DP[j][diff] == 0)
+                    continue;
+                DP[i][diff] = 1;
+                if(diff - 1 > 0)
+                    DP[i][diff - 1] = 1;
+                if(diff + 1 <= n)
+                    DP[i][diff + 1] = 1;
+            }
+        }
+        return count(DP.back().begin(), DP.back().end(), 1) > 1;
+    }
+};
