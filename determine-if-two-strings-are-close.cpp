098b757acd5bbb2161f8d22cd0a9483b74b43906
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-01-13 17:49:53 -0700

    Update determine-if-two-strings-are-close.cpp

--- determine-if-two-strings-are-close.cpp
+++ determine-if-two-strings-are-close.cpp
@@ -17,3 +17,25 @@ public:
         return count1 == count2 && s1 == s2;
     }
 };
+
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int> c1(26), c2(26);
+        for(char c: word1)
+            c1[c - 'a']++;
+        for(char c: word2)
+            c2[c - 'a']++;
+        priority_queue<int> pq1, pq2;
+        for(int i = 0; i < 26; i++)
+            if(c1[i])
+                pq1.push(c1[i]), pq2.push(c2[i]);
+        while(!pq1.empty() && !pq2.empty()) {
+            if(pq1.top() != pq2.top())
+                break;
+            pq1.pop(), pq2.pop();
+        }
+        return pq1.empty() && pq2.empty();
+    }
+};
