1bfc46ad4b947ca18490642d1b2530ffc9c9470d
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-01-16 17:16:05 -0700

    Update unique-number-of-occurrences.cpp

--- unique-number-of-occurrences.cpp
+++ unique-number-of-occurrences.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> umap;
+        vector<int> c(arr.size() + 1);
+        for(int &a : arr) {
+            umap[a]++;
+        }
+        for(auto &[k,v]: umap) {
+            if(c[v])
+                return false;
+            c[v]++;
+        }
+        return true;
+    }
+};
+
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int, int> count;
         for(int ele : arr) {
             count[ele]++;
