37201e0b91ff06cdb1f6f8da2921b9964f3266c6
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-01-19 20:07:37 -0700

    Update sum-of-subarray-minimums.cpp

--- sum-of-subarray-minimums.cpp
+++ sum-of-subarray-minimums.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         arr.insert(arr.begin(), 0);
+        arr.push_back(0);
+        long n = arr.size(), ans = 0;
+        stack<int> stk;
+        for(int i = 0; i < n; i++) {
+            while(!stk.empty() && arr[stk.top()] > arr[i]) {
+                int curr = stk.top();
+                stk.pop();
+                int left = stk.top();
+                int right = i;
+                ans += (curr - left) * (right - curr) * 1L * arr[curr];
+            }
+            stk.push(i);
+        }
+        return ans % 1000000007;
+    }
+};
+
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        arr.insert(arr.begin(), 0);
         stack<int> stk;
         stk.push(0);
         int n = arr.size();
