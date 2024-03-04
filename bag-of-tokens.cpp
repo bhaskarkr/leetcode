3bb0568dffbfcca731ce79d353851961859aea64
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-03-03 17:36:19 -0700

    Update bag-of-tokens.cpp

--- bag-of-tokens.cpp
+++ bag-of-tokens.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
         sort(tokens.begin(), tokens.end());
+        int ans = 0, curr = 0, i = 0, j = tokens.size() - 1;
+        while(i <= j) {
+            while(i <= j && tokens[i] <= power)
+                power -= tokens[i++], curr++;
+            ans = max(ans, curr);
+            if(curr > 0 && i < j)
+                power += tokens[j--], curr--;
+            else 
+                break;
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
         int i = 0, j = tokens.size() - 1;
         int curr = 0;
         int ans = 0;
@@ -21,3 +39,6 @@ public:
         return ans;
     }
 };
+
+
+
