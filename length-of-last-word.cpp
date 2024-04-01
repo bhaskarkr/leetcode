4fa601785ad11eeb8ca752a07e6fa3eaaf6767c3
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-03-31 18:23:23 -0700

    Update length-of-last-word.cpp

--- length-of-last-word.cpp
+++ length-of-last-word.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        int r = s.length() - 1;
+        while(r >= 0 && s[r] == ' ')
+            r--;
+        int l = r;
+        while(l >= 0 && s[l] != ' ')
+            l--;
+        return r - l;
+    }
+};
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
         int i = s.length() - 1;
         while(i >= 0 && !isalpha(s[i])) {
             i--;
