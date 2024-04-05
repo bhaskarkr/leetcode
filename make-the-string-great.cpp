e2e233256ccbb58b93b33eb0b419cc5b6d680e24
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-04-04 17:08:36 -0700

    Update make-the-string-great.cpp

--- make-the-string-great.cpp
+++ make-the-string-great.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     string makeGood(string s) {
+        string ans;
+        for(char &c : s) {
+            if(!ans.empty() && abs(ans.back() - c) == 32)
+                ans.pop_back();
+            else
+                ans.push_back(c);
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    string makeGood(string s) {
         string ans = "";
         for(char c : s) {
             if(ans.empty()) {
