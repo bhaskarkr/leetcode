5caff36528bddc3b90902d1c972c9f4a22e8ecf6
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-11-14 19:59:50 -0800

    Update asteroid-collision.cpp

--- asteroid-collision.cpp
+++ asteroid-collision.cpp
@@ -14,3 +14,27 @@ public:
         return stk;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int l = 0, r = asteroids.size() - 1;
+        vector<int> stk;
+        for(int i = 0; i < asteroids.size(); i++) {
+            if(stk.empty() || stk.back() * asteroids[i] > 0) {
+                stk.push_back(asteroids[i]);
+            } else {
+                while(!stk.empty() and stk.back() > 0 and stk.back() < -asteroids[i]) {
+                    stk.pop_back();
+                }
+                if(!stk.empty() and stk.back() == abs(asteroids[i])) {
+                    stk.pop_back();
+                }
+                else if(stk.empty() or stk.back() < 0)
+                    stk.push_back(asteroids[i]);
+            }
+        }
+        return stk;
+    }
+};
