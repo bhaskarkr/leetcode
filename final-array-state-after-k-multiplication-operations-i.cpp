481282ed32228ad8f993afc9e35d84b81c49ef6d
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2024-12-15 17:07:03 -0800

    Update final-array-state-after-k-multiplication-operations-i.cpp

--- final-array-state-after-k-multiplication-operations-i.cpp
+++ final-array-state-after-k-multiplication-operations-i.cpp
@@ -19,3 +19,21 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+        }
+        while(k--) {
+            auto top = pq.top();
+            pq.pop();
+            top.first *= multiplier; 
+            nums[top.second] = top.first;
+            pq.push(top);
+        }
+        return nums;
+    }
+};
