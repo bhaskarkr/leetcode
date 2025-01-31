83d19d3cdd8e2caa07807c738cbea89cb92d261f
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-01-30 23:34:49 -0800

    Update maximum-score-after-splitting-a-string.cpp

--- maximum-score-after-splitting-a-string.cpp
+++ maximum-score-after-splitting-a-string.cpp
@@ -12,3 +12,22 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int ones = 0, zero = s[0] == '0', ans = 0;
+        for(int i = 1; i < s.length(); i++)
+            ones += s[i] == '1';
+        ans = max(ans, ones + zero);
+        for(int i = 1; i < s.length() - 1; i++) {
+            if(s[i] == '1')
+                ones--;
+            else
+                zero++;
+            ans = max(ans, ones + zero);
+        }
+        return ans;
+    }
+};
