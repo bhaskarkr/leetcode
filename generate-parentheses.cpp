1649a1ed600f96608d9265a208900d580f7dcb7c
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-02-04 08:52:00 -0800

    Update generate-parentheses.cpp

--- generate-parentheses.cpp
+++ generate-parentheses.cpp
@@ -17,3 +17,23 @@ public:
         dfs(n - 1, str + ')', open - 1, ans);   
     }
 };
+
+
+class Solution {
+public:
+    void dfs(int n, string s, int open, int close, vector<string> &ans) {
+        if(s.length() >= 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+        if(open < n)
+            dfs(n, s + '(', open + 1, close, ans);
+        if(open > close)
+            dfs(n, s + ')', open, close + 1, ans);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        dfs(n, "", 0, 0, ans);
+        return ans;
+    }
+};
