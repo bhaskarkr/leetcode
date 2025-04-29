75c630d26ea39c95688965897119e7909de9d768
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-04-28 19:50:57 -0700

    Update count-subarrays-with-score-less-than-k.cpp

--- count-subarrays-with-score-less-than-k.cpp
+++ count-subarrays-with-score-less-than-k.cpp
@@ -22,3 +22,19 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        long long n = nums.size(), ans = 0, sum = 0;
+        for(long long right = 0, left = 0; right < n; right++) {
+            sum += nums[right];
+            while(sum * (right - left + 1) >= k && left <= right) {
+                sum -= nums[left++];
+            }
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+};
