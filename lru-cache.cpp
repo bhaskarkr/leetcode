8462b9c28e68d7ffd642923bd726dc4a3e2645ab
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-06-18 19:53:31 -0700

    Update lru-cache.cpp

--- lru-cache.cpp
+++ lru-cache.cpp
@@ -38,3 +38,45 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+
+class LRUCache {
+public:
+    list<pair<int, int>> list_pair;
+    unordered_map<int, list<pair<int, int>> :: iterator> umap;
+    int max_capacity = 0;
+    LRUCache(int capacity) {
+        max_capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(umap.find(key) == umap.end()) {
+            return -1;
+        }
+        auto val = *umap[key];
+        list_pair.erase(umap[key]);
+        list_pair.push_front({key, val.second});
+        umap[key] = list_pair.begin();
+        return val.second;
+    }
+    
+    void put(int key, int value) {
+        if(umap.find(key) != umap.end()) {
+            list_pair.erase(umap[key]);
+        } 
+        else if(umap.size() >= max_capacity) {
+            auto temp = list_pair.back().first;
+            list_pair.pop_back();
+            umap.erase(temp);
+        }
+        list_pair.push_front({key, value});
+        umap[key] = list_pair.begin();
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
