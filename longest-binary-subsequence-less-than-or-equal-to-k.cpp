15a675685b6188727fd230ff37bfd78bce95a6dc
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-06-26 21:44:34 -0700

    Update longest-binary-subsequence-less-than-or-equal-to-k.cpp

--- longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -14,3 +14,20 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        long long ans = 0, sum = 0, pow = 1;
+        int zeroes = count(s.begin(), s.end(), '0');
+        for(long r = s.length() - 1; r >= 0 && sum + pow <= k; r--) {
+            if(s[r] == '1' && sum + pow <= k) {
+                sum += pow;
+                ans++;
+            }
+            pow <<= 1;
+        }
+        return ans + zeroes;
+    }
+};
