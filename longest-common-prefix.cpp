926858a8c41070a4d44d5afeae39809a0b25ddd3
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-07-17 20:03:39 -0700

    Update longest-common-prefix.cpp

--- longest-common-prefix.cpp
+++ longest-common-prefix.cpp
@@ -14,3 +14,19 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        for(int i = 0; i < strs[0].length(); i++) {
+            for(int j = 1; j < strs.size(); j++) {
+                if(i >= strs[j].length() or strs[j][i] != strs[j - 1][i])
+                    return ans;
+            }
+            ans.push_back(strs[0][i]);
+        }
+        return ans;
+    }
+};
