d07c9e0a9044207bda6d4fc2f737f7eefe6c79ac
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-11-25 00:09:27 -0800

    Update set-intersection-size-at-least-two.cpp

--- set-intersection-size-at-least-two.cpp
+++ set-intersection-size-at-least-two.cpp
@@ -21,3 +21,26 @@ public:
         return ans.size();
     }
 };
+
+
+class Solution {
+public:
+    bool static cmp(vector<int> a, vector<int> b) {
+        if(a[1] == b[1])
+            return a[0] > b[0];
+        return a[1] < b[1];
+    }
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), cmp);
+        vector<int> ans;
+        for(auto interval : intervals) {
+            if(ans.empty() || ans.back() < interval[0]) {
+                ans.push_back({interval[1] - 1});
+                ans.push_back({interval[1]});
+            } else if(ans[ans.size() - 2] < interval[0]) {
+                ans.push_back({interval[1]});
+            }
+        }
+        return ans.size();
+    }
+};
