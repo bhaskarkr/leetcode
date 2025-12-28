64ee9f2f7d959a2a0f6acc7d16869c55283a88da
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2025-12-27 21:50:12 -0800

    Update largest-number.cpp

--- largest-number.cpp
+++ largest-number.cpp
@@ -14,3 +14,19 @@ public:
         return ans[0] == '0'? "0" : ans ;
     }
 };
+
+
+class Solution {
+public:
+    string largestNumber(vector<int> nums) {
+        auto lambda = [](int a, int b) {
+            return to_string(a) + to_string(b) > to_string(b) + to_string(a);
+        };
+        sort(nums.begin(), nums.end(), lambda);
+        string ans;
+        for(auto n : nums)
+            ans += to_string(n);
+        unordered_set<char> uset(ans.begin(), ans.end());
+        return uset.size() == 1 and *uset.begin() == '0' ? "0" : ans;
+    }
+};
