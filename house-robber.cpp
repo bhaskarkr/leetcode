332b511e2528cf04368e809af37b080abc661c53
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-01-17 09:49:30 -0800

    Update house-robber.cpp

--- house-robber.cpp
+++ house-robber.cpp
@@ -6,3 +6,14 @@ public:
         return nums.back();
     }
 };
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> DP(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++) {
+            DP[i] = max((i >= 2 ? DP[i-2] : 0) + DP[i], DP[i-1]);
+        }
+        return DP.back();
+    }
+};
