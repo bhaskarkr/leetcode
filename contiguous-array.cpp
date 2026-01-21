5825b59e00d3ad42c739ca8e6d5480b60f8e26fa
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-01-20 20:13:55 -0800

    Update contiguous-array.cpp

--- contiguous-array.cpp
+++ contiguous-array.cpp
@@ -14,3 +14,22 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> umap;
+        umap[0] = -1;
+        int sum = 0, ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            sum += num == 1 ? 1 : -1;
+            if(umap.find(sum) == umap.end()) {
+                umap[sum] = i;
+            } else {
+                ans = max(ans, i - umap[sum]);
+            }
+        }
+        return ans;
+    }
+};
