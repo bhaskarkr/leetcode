b96c6ecb25af05498349f93fb887007079406dfc
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-01-24 15:00:52 -0800

    Create minimum-edge-reversals-so-every-node-is-reachable.cpp

---
+++ minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
+        unordered_map<int, unordered_map<int, int>> umap;
+        unordered_map<int, unordered_map<int, int>> dp;
+        for(auto &edge : edges) {
+            umap[edge[0]][edge[1]] = 0;
+            umap[edge[1]][edge[0]] = 1;
+        }
+
+        function<int(int, int)> dfs = [&](int parent, int curr) {
+            if(dp.find(parent) != dp.end() && dp[parent].find(curr) != dp[parent].end()) {
+                return dp[parent][curr];
+            }
+            dp[parent][curr] = 0;
+            for(auto &[child, cost] : umap[curr]) {
+                if(child == parent)
+                    continue;
+                dp[parent][curr] += dfs(curr, child) + cost;
+            }
+            return dp[parent][curr];
+        };
+
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            ans.push_back(dfs(-1, i));
+        }
+        return ans;
+    }
+};
