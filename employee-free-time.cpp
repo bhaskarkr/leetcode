1515c757cd9024fcd26c298e73b6125f6c0f601a
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-01 13:14:46 -0800

    Update employee-free-time.cpp

--- employee-free-time.cpp
+++ employee-free-time.cpp
@@ -41,3 +41,51 @@ public:
         return result;
     }
 };
+
+
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+        vector<Interval> temp;
+        for(auto emp : schedule) {
+            for(auto slot : emp) {
+                temp.push_back(slot);
+            }
+        }
+        sort(temp.begin(), temp.end(), [&](auto a, auto b) {
+            return a.start < b.start;
+        });
+
+        vector<Interval> merged;
+        merged.push_back(temp[0]);
+        for(int i = 1; i < temp.size(); i++) {
+            if(merged.back().end >= temp[i].start) {
+                merged.back().end = max(merged.back().end, temp[i].end);
+            } else {
+                merged.push_back(temp[i]);
+            }
+        }
+
+        vector<Interval> ans;
+        for(int i = 1; i < merged.size(); i++) {
+            ans.push_back(Interval(merged[i-1].end, merged[i].start));
+        }
+        return ans;
+    }
+};
