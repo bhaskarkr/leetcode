22e66b5f3d71113f5053fbec1480cf974f78f1d2
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-05 11:01:49 -0800

    Create critical-connections-in-a-network.cpp

---
+++ critical-connections-in-a-network.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void tarjan(int curr, int parent, int time, vector<vector<int>>& AdjLis, vector<int>& low, vector<vector<int>> &ans){
+        low[curr] = time++;
+        for (auto x: AdjLis[curr]){
+            if (x==parent)
+                continue;
+            if (low[x]==0) tarjan(x, curr, time, AdjLis, low, ans);
+            low[curr] = min(low[curr], low[x]);
+            if (low[x]==time)
+                ans.push_back({curr,x});
+        }
+    }
+
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+        for (auto x: connections){
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+        }
+        vector<vector<int>> ans;
+        vector<int> low(n);
+        tarjan(0, -1, 1, adj, low, ans);
+        return ans;
+    }
+};
