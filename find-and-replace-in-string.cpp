195529cc258bc275ed72a05c7a20ca5c1d546c48
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-07 14:57:10 -0800

    Update find-and-replace-in-string.cpp

--- find-and-replace-in-string.cpp
+++ find-and-replace-in-string.cpp
@@ -17,3 +17,21 @@ public:
         return s;
     }
 };
+
+
+class Solution {
+public:
+    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
+        int n = indices.size();
+        vector<tuple<int, string, string>> inp;
+        for(int i = 0; i < n; i++)
+            inp.push_back(make_tuple(indices[i], sources[i], targets[i]));
+        sort(inp.begin(), inp.end());
+        for(int i = n - 1; i >= 0; i--) {
+            auto [index, source, target] = inp[i];
+            if(index + source.length() <= s.length() && s.substr(index, source.length()) == source)
+                s = s.substr(0, index) + target + s.substr(index + source.length());
+        }
+        return s;
+    }
+};
