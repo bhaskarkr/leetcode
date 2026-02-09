63105cb3fa4688d2d1a804bc40cae02520b98f95
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-08 21:57:29 -0800

    Create minimum-deletions-to-make-string-balanced.cpp

---
+++ minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        vector<int> DP(s.length() + 1);
+        int b = 0;
+        for(int i = 1; i <= s.length(); i++) {
+            if(s[i-1] == 'b') {
+                DP[i] = DP[i-1];
+                b++;
+            } else {
+                DP[i] = min(b, DP[i-1] + 1);
+            }
+        }
+        return DP.back();
+    }
+};
