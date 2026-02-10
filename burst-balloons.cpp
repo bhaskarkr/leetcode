12eeda35e01364a9e700435e6971f3a204b613cc
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-10 15:23:31 -0800

    Update burst-balloons.cpp

--- burst-balloons.cpp
+++ burst-balloons.cpp
@@ -22,3 +22,28 @@ public:
         return helper(nums, 1, n - 1, dp);
     }
 };
+
+class Solution { // chnaged the indices 
+public:
+    int helper(vector<int> &nums, int l, int r, vector<vector<int>> &dp) {
+        if(l > r)
+            return 0;
+        if(dp[l][r] != -1)
+            return dp[l][r];
+        int maxSum = 0;
+        for(int k = l; k <= r; k++) {
+            // cout<<k<<endl;
+            int currSum = helper(nums, l, k-1, dp) + helper(nums, k+1, r, dp) + nums[l - 1] * nums[k] * nums[r+1]; 
+            maxSum = max(maxSum, currSum);
+            dp[l][r] = maxSum;
+        }
+        return dp[l][r];
+    }
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        return helper(nums, 1, n - 2, dp);
+    }
+};
