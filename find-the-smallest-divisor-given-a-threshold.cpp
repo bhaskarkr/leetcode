68021c85cd64efaf50de0bd3bcde93b2fd3ee28e
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-17 12:36:59 -0800

    Create find-the-smallest-divisor-given-a-threshold.cpp

---
+++ find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int l = 1, r = *max_element(nums.begin(), nums.end());
+        while(l < r) {
+            int m = l + (r - l) / 2;
+            int sum = 0;
+            for(int num : nums) {
+                sum += (num + m - 1) / m;
+            }
+            if(sum > threshold) {
+                l = m + 1;
+            } else 
+                r = m;
+        }
+        return l;
+    }
+};
