c5f401402c7b7702e8951183c95bb6d8e66027fb
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-17 21:39:41 -0800

    Update wildcard-matching.cpp

--- wildcard-matching.cpp
+++ wildcard-matching.cpp
@@ -22,3 +22,23 @@ public:
         return memo[getKey(i, j)] = 0;
     }
 };
+
+class Solution {
+public:
+    int DP[2001][2001];
+    int helper(int i, int j, string &s, string &p) {
+        if(j == p.length())
+            return i == s.length() ? 1 : 0;
+        if(DP[i][j] != -1)
+            return DP[i][j];
+        int curr = (i < s.length() && (p[j] == '?' || s[i] == p[j]) && helper(i + 1, j + 1, s, p));
+        if(p[j] == '*') {
+            curr |= (i < s.length() && helper(i + 1, j, s, p)) || helper(i, j + 1, s, p);
+        } 
+        return DP[i][j] = curr;
+    }
+    bool isMatch(string s, string p) {
+        memset(DP, -1, sizeof(DP));
+        return helper(0, 0, s, p) == 1;
+    }
+};
