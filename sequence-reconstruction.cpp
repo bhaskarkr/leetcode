bffac7269d3ea95d51f8987d6a38311a843d008f
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-18 12:51:17 -0800

    Update sequence-reconstruction.cpp

--- sequence-reconstruction.cpp
+++ sequence-reconstruction.cpp
@@ -22,3 +22,29 @@ public:
         return true;
     } 
 };
+
+
+class Solution {
+public:
+    bool sequenceReconstruction(vector<int>& nums, vector<vector<int>>& sequences) {
+        unordered_map<int, int> index;
+        unordered_set<long long> pairs;
+        for(int i = 0; i < nums.size(); i++) {
+            index[nums[i]] = i + 1;
+        }
+            
+        for(auto seq : sequences) {
+            for(int i = 1; i < seq.size(); i++) {
+                if(index[seq[i-1]] > index[seq[i]] )
+                    return false;
+                pairs.insert(seq[i-1]*10e5+seq[i]);
+            }
+        }
+        
+        for(int i = 1; i < nums.size(); i++) {
+           if(pairs.find(nums[i-1]*10e5+nums[i]) == pairs.end())
+               return false;    
+        }
+        return true;
+    } 
+};
