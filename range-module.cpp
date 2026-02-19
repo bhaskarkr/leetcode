65fd3bac582c3692f7cd914a728100e769c821e0
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-19 11:45:40 -0800

    Update range-module.cpp

--- range-module.cpp
+++ range-module.cpp
@@ -50,3 +50,52 @@ public:
  * bool param_2 = obj->queryRange(left,right);
  * obj->removeRange(left,right);
  */
+
+
+class RangeModule {
+public:
+    map<int, int> mp;
+    RangeModule() {
+        
+    }
+    
+    void addRange(int left, int right) {
+        auto nextRight = mp.upper_bound(right);
+        while(nextRight != mp.begin() && (prev(nextRight)->second) >= left) {
+            left = min(left, prev(nextRight)->first);
+            right = max(right, prev(nextRight)->second);
+            mp.erase(prev(nextRight));
+        }
+        mp[left] = right;
+    } 
+    
+    bool queryRange(int left, int right) {
+        auto itr = mp.upper_bound(left);
+        return itr != mp.begin() && prev(itr)->second >= right;
+    }
+    
+    void removeRange(int left, int right) {
+        auto itr = mp.lower_bound(left);
+        int finalRight= -1;
+        while(itr != mp.end() && (itr->first) <= right) {
+            finalRight = max(finalRight, itr->second);
+            itr++;
+            mp.erase(prev(itr));
+        }
+        if(itr != mp.begin() && prev(itr)->second >= left) {
+            finalRight = max(finalRight, prev(itr)->second);
+            prev(itr)->second = left;
+        }
+        if(right < finalRight) {
+            mp[right] = finalRight;
+        }
+    }
+};
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
