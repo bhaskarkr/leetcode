55d11820b5ba87ade1e26fb9a4ce648ca3f49e38
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-19 12:18:41 -0800

    Update minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp

--- minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -27,3 +27,14 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int ans = target[0];
+        for(int i = 1; i < target.size(); i++) {
+            ans += target[i] > target[i-1] ? target[i] - target[i-1] : 0;
+        }
+        return ans;
+    }
+};
