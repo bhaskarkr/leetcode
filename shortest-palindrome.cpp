b53c3ff71611f34bf35e4697f582652724f7bb6a
Bhaskar Kumar <officialbhaskarkumar@gmail.com>
2026-02-19 18:14:24 -0800

    Update shortest-palindrome.cpp

--- shortest-palindrome.cpp
+++ shortest-palindrome.cpp
@@ -12,3 +12,18 @@ class Solution {
         return ss;
     }
 };
+
+class Solution {
+ public:
+    string shortestPalindrome(string ss) {
+        string rr = ss;
+        reverse(rr.begin(), rr.end());
+        string_view s(ss);
+        string_view r(rr);
+        for(int i = 0; i < s.length(); i++) {
+            if(s.starts_with(r.substr(i)))
+                return rr.substr(0, i) + ss;
+        }
+        return ss;
+    }
+};
